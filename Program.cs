93044911ffa6d41cf7dae1b85342c1470395e542
adewalecodesbug <adewalecodesbug@gmail.com>
2023-01-16 07:33:21 +0100

    A simple symbol sorting algorithm with nested for loops

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sorting
+{
+    internal abstract class Code
+    {
+        private static void Main()
+        {
+            Console.Write("How many rows?: ");
+            int rows = Convert.ToInt16(Console.ReadLine());
+            
+            Console.Write("How many columns?: ");
+            int columns = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("What symbol?: ");
+            char sign = Convert.ToChar(Console.Read());
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
